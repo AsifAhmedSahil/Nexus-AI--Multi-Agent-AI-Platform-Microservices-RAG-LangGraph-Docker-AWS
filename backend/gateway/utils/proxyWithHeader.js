e7a00cfd5fd961677f17ad23f585b008dfefdeaa
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2026-09-16 21:10:26 +0600

    update timeout issue

--- backend/gateway/utils/proxyWithHeader.js
+++ backend/gateway/utils/proxyWithHeader.js
@@ -4,6 +4,7 @@ import proxy from "express-http-proxy"
 export const proxyWithHeader = (serviceUrl, options = {}) =>{
     return proxy(serviceUrl,{
         ...options,
+        timeout: 300000,
         proxyReqOptDecorator:(proxyReqOpts,srcReq)=>{
             if(srcReq.user){
                 proxyReqOpts.headers["x-user-id"] = srcReq.user.userId
