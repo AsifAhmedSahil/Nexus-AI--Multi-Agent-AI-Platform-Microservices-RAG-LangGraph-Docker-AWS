4173d37800f3d400c4e9fd792c2e60b25e9207af
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2026-09-16 16:21:07 +0600

    update pipeline

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,8 +1,17 @@
-import  { cert, initializeApp } from "firebase-admin"
+import { cert, initializeApp } from "firebase-admin";
 
-import serviceAccount from "../serviceAccountKey.json" with {type:"json"}
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+};
 
 export const app = initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert(serviceAccount),
 });
 
